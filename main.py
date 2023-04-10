bc15e5c6254a2b1268e0d683e08257e970f368f3
David Ordoñez <davidordonezescacena@hotmail.es>
2023-04-10 20:01:42 +0200

    comments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import deck
 
+# create deck
 deck_1 = deck.create_deck()
+
+# shuffle deck
 deck_shuffle = deck.shuffle(deck_1)
+
+# hands of the players, (deck to be dealt, number of players, number of decks)
 hands = deck.deal(deck_shuffle, 4, 5)
 
 print(hands)
